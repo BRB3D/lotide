e20f777a2c92b64c2d12cbda6f1c98bef55db393
BRB3D <samuraix221@hotmail.com>
2021-10-26 21:16:57 -0400

    refactored function with previous version commented out

--- without.js
+++ without.js
@@ -8,19 +8,23 @@ const assertArraysEqual =  (arr1, arr2) => {
 };
 
 const without =  (arr1, arr2) => {
-  let final = [...arr1];
-  for (let i = 0; i < arr2.length; i++) {
-    while (final.indexOf(arr2[i]) > -1) {
-      final.splice(final.indexOf(arr2[i]),1);
+  /* let final = [...arr1]; */
+  let final = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      final.push(arr1[i]);
     }
+    /* while (final.indexOf(arr2[i]) > -1) {
+      final.splice(final.indexOf(arr2[i]),1);
+    } */
   }
   return final;
 };
 let words = [1, 2, 3];
 let words2 = ["1", "2", "3"];
 let words3 = ['1','a',3];
-console.log(without(words , [1]));
+assertArraysEqual(without(words , [1]), [2,3]);
 assertArraysEqual(words , [1, 2, 3]);
-console.log(without(words2 , [1, 2, "3"]));
+assertArraysEqual(without(words2 , [1, 2, "3"]),['1', '2']);
 assertArraysEqual(words2 , ["1", "2", "3"]);
-console.log(without(words3, ['a']));
+assertArraysEqual(without(words3, ['a']), ['1', 3]);
