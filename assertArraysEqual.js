710461ab72748ee19ea38dfc2717a0b685c24fae
BRB3D <samuraix221@hotmail.com>
2021-10-26 22:03:18 -0400

    fixed a typo the name of the function is now assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,5 @@
 
-const eqArrays =  (arr1, arr2) => {
+const assertArraysEqual =  (arr1, arr2) => {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return console.log(`❌ Failed: ${arr1} !== ${arr2}`);
@@ -8,7 +8,7 @@ const eqArrays =  (arr1, arr2) => {
   return console.log(`✅ Passed: ${arr1} === ${arr2}`);
 };
 
-eqArrays([1, 2, 3], [3, 2, 1]);
-eqArrays([1, 2, 3], [1, 2, 3]);
-eqArrays(["1", "2", "3"], ["1", "2", "3"]);
-eqArrays(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
