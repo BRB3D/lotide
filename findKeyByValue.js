4a3ce99c3cb6c1c1b7c9076ce27cf3861349f9ef
BRB3D <samuraix221@hotmail.com>
2021-10-27 20:56:02 -0400

    function that looks through the values of an object and returns the key that holds the value.

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  return actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (object, value) => {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key; 
+    }
+  }
+  return undefined;
+}
+
+
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Big Bang Theory"), "comedy");
