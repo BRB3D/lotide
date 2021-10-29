32e99fd1b5ccef22f440c7858bcd895e8054a1e7
BRB3D <samuraix221@hotmail.com>
2021-10-28 21:13:10 -0400

    function with for in that stops at the first match and returns the key. Test cases included

---
+++ findKey.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  return actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = (obj, match) => {
+  for (let key in obj){
+    if (match(obj[key])) {
+      
+      return key; 
+    }
+  }
+};
+
+// Example of what it should do. 
+let result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+let result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5) // => "undefined"
+
+let result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3) // => "Akelarre"
+
+assertEqual(result, 'noma');
+assertEqual(result2, undefined);
+assertEqual(result3, 'Akaleri');
