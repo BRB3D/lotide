43ccf1e9a7153032d1e2fd6fd3c6be04fe70d7e2
BRB3D <samuraix221@hotmail.com>
2021-10-28 20:11:54 -0400

    Corrected eqArrays now used inside the asserArrayEquals instead of the logic being inside only

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,21 @@
 
-const assertArraysEqual =  (arr1, arr2) => {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return console.log(`❌ Failed: ${arr1} !== ${arr2}`);
-    }
+const assertArraysEqual =  (arr1, arr2, eqArray) => {
+  if (eqArray(arr1, arr2)) {
+    return console.log(`✅ Passed: ${arr1} === ${arr2}`);
   }
-  return console.log(`✅ Passed: ${arr1} === ${arr2}`);
+  return console.log(`❌ Failed: ${arr1} !== ${arr2}`);
 };
 
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+const eqArrays =  (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertArraysEqual([1, 2, 3], [3, 2, 1], eqArrays);
+assertArraysEqual([1, 2, 3], [1, 2, 3], eqArrays);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"], eqArrays);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3], eqArrays);
