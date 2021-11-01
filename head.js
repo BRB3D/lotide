541f3e633323d97ad142b5c983bd213517b3e0d4
BRB3D <samuraix221@hotmail.com>
2021-11-01 14:12:49 -0400

    eslint head & refactored tail with an included test file in the test folder

--- head.js
+++ head.js
@@ -3,4 +3,4 @@ const head = arr => arr.length > 0 ? arr[0] : undefined;
 // Test cases for Head.js
 
 
-module.exports = {head, assertEqual}; 
+module.exports = {head, assertEqual};
--- tail.js
+++ tail.js
@@ -1,17 +1,6 @@
-const assertEqual = function(actual, expected) {
-  return actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 const tail = arr => arr.length > 0 ? arr.slice(1) : [];
 
-const result = tail(["Hello", "Lighthouse", 'Labs']);
-assertEqual(result.join(), ["Lighthouse", "Labs"].join());
-/* // Test Case: Check the original array
-const result = tail(["Hello", "Lighthouse", 3]);
-assertEqual(result.join(), ["Lighthouse", "Labs"].join());
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
-const result = tail([]);
-assertEqual(result.join(), ["Lighthouse", "Labs"].join());
-*/
+
+module.exports = {tail, assertEqual};
