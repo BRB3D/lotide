a4c7a93177ef2793f276fe53fa19c27d28ab9da7
BRB3D <samuraix221@hotmail.com>
2021-11-01 13:57:52 -0400

    Refactored findKey with a more readable code

--- findKey.js
+++ findKey.js
@@ -3,15 +3,14 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKey = (obj, match) => {
-  for (let key in obj){
+  for (let key in obj) {
     if (match(obj[key])) {
-      
-      return key; 
+      return key;
     }
   }
 };
 
-// Example of what it should do. 
+// Example of what it should do.
 let result = findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
@@ -19,7 +18,7 @@ let result = findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2); // => "noma"
 
 let result2 = findKey({
   "Blue Hill": { stars: 1 },
@@ -28,7 +27,7 @@ let result2 = findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 5) // => "undefined"
+}, x => x.stars === 5); // => "undefined"
 
 let result3 = findKey({
   "Blue Hill": { stars: 1 },
@@ -37,7 +36,7 @@ let result3 = findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 3) // => "Akelarre"
+}, x => x.stars === 3); // => "Akelarre"
 
 assertEqual(result, 'noma');
 assertEqual(result2, undefined);
