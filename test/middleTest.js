fa7c9dbc485c6894e2ed6dec66879623f2b62830
BRB3D <samuraix221@hotmail.com>
2021-11-01 19:45:09 -0400

    Changed the tetse format to use chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,27 @@
 const {assertArraysEqual, eqArrays} = require('../assertArraysEqual');
-
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([0,1,2,3,4,5,6]), [3], eqArrays);
-assertArraysEqual(middle([0,1,2,3,4,5]), [2,3], eqArrays);
-assertArraysEqual(middle([0,1]), [], eqArrays);
-assertArraysEqual(middle([0,1,2]), [1], eqArrays);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4], eqArrays);
+
+
+describe('Returns middle of a word', function() {
+  it('Returns [3] from [0,1,2,3,4,5,6] becasue odd numbers return one middle number', function() {
+
+  assert.deepEqual(middle([0,1,2,3,4,5,6]), [3]);
+  });
+  it('Returns [2,3] from [0,1,2,3,4,5] becasue even numbers return two numbers', function() {
+
+  assert.deepEqual(middle([0,1,2,3,4,5]), [2,3]);
+  });
+
+  it('Returns [] from [0,1] arrays smaller than two values return an empty array', function() {
+
+  assert.deepEqual(middle([0,1]), []);
+  });
+
+  it('Returns [1] from [0,1,2] becasue odd numbers return one middle number', function() {
+
+    assert.deepEqual(middle([0,1,2]), [1]);
+    });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,24 @@
 const {assertEqual, tail} = require('../tail');
+const assert = require('chai').assert; 
 
 
-const result2 = tail(["Hello", "Lighthouse", 'Labs']);
-assertEqual(result2.join(), ["Lighthouse", "Labs"].join());
 
-const result = tail(["Hello", "Lighthouse", 3]);
-assertEqual(result.join(), ["Lighthouse", "Labs"].join());
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
-const result3 = tail([]);
-assertEqual(result3.join(), ["Lighthouse", "Labs"].join());
+
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", 'Labs']), ["Lighthouse", "Labs"]);
+  });
+  
+  it('returns ["Labs", 3] for ', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", 3]), ["Lighthouse", 3]);
+  });
+ 
+  it('returns  3 for  words.length ', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words)
+    assert.strictEqual(words.length, 3);
+  });
+
+
+});
